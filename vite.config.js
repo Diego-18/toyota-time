49b9c1586c704b1b4ae6e1ad8cd6718196170ac5
Diego-18 <ingdiegochavez18@gmail.com>
2023-07-10 23:08:49 -0500

    add-dockerfiles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  jsxInject: `import React from 'react'`,
-  plugins: [react()]
-})
+	jsxInject: `import React from 'react'`,
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
+});
